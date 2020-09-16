6fe59b03ab6df91ffe2c6ac9359bf8cea412d840
EvgeniyKovalenko <evgeniy.kovalenko@itdecision-llc.com>
2020-09-16 13:41:29 +0300

    v1.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const localServer = {
     path: 'localhost/dist/',
     port: 3000,
-  };
+};
 
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -11,11 +11,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
     entry: {
         app: './src/js/app.js',
-      },
-      output: {
+    },
+    output: {
         filename: 'js/[name].js',
         path: path.resolve(__dirname, 'dist'),
-      },
+    },
     module: {
         rules: [
             {
@@ -44,34 +44,34 @@ const config = {
                 }
             },
             {
-              test: /\.scss$/,
-              use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
             },
         ],
     },
-      plugins: [
+    plugins: [
         new HtmlWebpackPlugin({
             inject: true,
             hash: false,
             filename: 'index.html',
             template: path.resolve(__dirname, 'src', 'index.html')
-          }),
+        }),
         new HtmlWebpackPlugin({
             inject: true,
             hash: false,
             filename: 'index2.html',
             template: path.resolve(__dirname, 'src', 'index2.html')
-          }),
+        }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
-          }),
-          new CopyWebpackPlugin({
-              patterns: [
-                  { from: path.resolve(__dirname, 'src', 'images'), to: path.resolve(__dirname, 'dist', 'images')},
-                  { from: path.resolve(__dirname, 'src', 'fonts'), to: path.resolve(__dirname, 'dist', 'fonts')},
-              ]
-          })
-      ]
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, 'src', 'images'), to: path.resolve(__dirname, 'dist', 'images')},
+                {from: path.resolve(__dirname, 'src', 'fonts'), to: path.resolve(__dirname, 'dist', 'fonts')},
+            ]
+        })
+    ]
 
 };
 
